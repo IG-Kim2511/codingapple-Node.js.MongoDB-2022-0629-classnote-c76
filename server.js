5688b9248a4cd6693f0d19533a5dc6aa8f55ab9a
IG <iikim2511@gmail.com>
2022-07-22 19:45:12 -0400

    clear

--- server.js
+++ server.js
@@ -334,7 +334,7 @@ app.get('/detail',(req요청,res응답)=>{
 
 
 // // 🦄🦄c56 (회원 로그인0) 세션, JWT, OAuth 등 회원인증 방법 이해하기
-// // 🦄🦄c58 (회원 로그인1) app.use(~), passport, passport-local, express-session, passport.authenticate(~),passport.use(new LocalStorategy(~))
+// // 🦄🦄c58 (회원 로그인1) 미들웨어, app.use(~), passport, express-session, passport.authenticate(~),passport.use(new LocalStorategy(~))
 
 // // 🦄🦄c60 (회원 로그인2) 아이디 비번을 DB와 비교하고 세션 만들어주기, passport.serializeUser(~)
 // // 🦄🦄c62 (회원 로그인3) 로그인 유저만 접속할 수 있는 페이지 만들기
